00784f92ae10fed251cdbcacb81febc919dcf140
William Lines <williamlines42@gmail.com>
2022-10-21 11:00:01 +0100

    started work on building interactive menu

--- lib/interactive_menu.rb
+++ lib/interactive_menu.rb
@@ -1,5 +1,33 @@
   
 class InteractiveMenu  
+  def initialize
+    @menu = {
+      prawn_curry: 550,
+      chilli_beef: 550,
+      egg_fried_rice: 300,
+      boiled_rice: 240,
+      chicken_chow_mein: 430
+    }
+  end
+  
+  def format_symbol(symbol)
+    symbol.to_s.capitalize.split("_").join(" ")
+  end
+
+  def format_price(price)
+    pounds = (price / 100.0)
+    pounds = sprintf('%.2f', pounds)
+    return "£#{pounds}"
+  end
+
+  def display_menu
+    menu_string = "Our menu:"
+    @menu.each do |item, price|
+      menu_string << "\n#{format_symbol(item)} -- #{format_price(price)}"
+    end
+    return menu_string
+  end
+  
   def view_order 
     "Order is empty: add something!"
   end
--- spec/interactive_menu_spec.rb
+++ spec/interactive_menu_spec.rb
@@ -1,1 +1,22 @@
 require "interactive_menu"
+
+RSpec.describe InteractiveMenu do
+  it "can display the menu" do
+    menu = InteractiveMenu.new
+    expect(menu.display_menu).to eq("Our menu:\nPrawn curry -- £5.50\nChilli beef -- £5.50\nEgg fried rice -- £3.00\nBoiled rice -- £2.40\nChicken chow mein -- £4.30")
+  end
+  context "testing formatters" do
+    it "testing symbol formatter" do
+      menu = InteractiveMenu.new
+      expect(menu.format_symbol(:prawn_curry)).to eq "Prawn curry"
+      expect(menu.format_symbol(:this_symbol_is_way_longer_than_usual)).to eq "This symbol is way longer than usual"
+    end
+    it "testing price formatter" do
+      menu = InteractiveMenu.new
+      expect(menu.format_price(100)).to eq "£1.00"
+      expect(menu.format_price(550)).to eq "£5.50"
+      expect(menu.format_price(123)).to eq "£1.23"
+      expect(menu.format_price(10033)).to eq "£100.33"
+    end
+  end
+end
